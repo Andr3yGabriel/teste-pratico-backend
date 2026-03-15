2c3f8583dc9dfb28a2f757d07d3daa996f835474
Andr3yGabriel <andreygoncalvesdev@gmail.com>
2026-03-15 17:48:33 -0300

    tests: Adiciona teste com falha para rota de checkout

---
+++ payment-api/tests/functional/functional.spec.ts
@@ -0,0 +1,33 @@
+import { test } from '@japa/runner'
+
+test.group('Checkout Transactions', (group) => {
+
+  test('should process a checkout transaction successfully via Gateway', async ({ client }) => {
+    const payload = {
+      "products": [
+        {
+          "product_id": 1,
+          "quantity": 2
+        },
+        {
+          "product_id": 2,
+          "quantity": 1
+        }
+      ],
+      "client": {
+        "name": "tester",
+        "email": "user@example.com",
+        "cardNumber": "5569000000006063",
+        "cvv": "010"
+      }
+    }
+
+    const response = await client.post('/checkout').json(payload)
+
+    response.assertStatus(201)
+    response.assertBodyContains({
+      message: 'Checkout successful',
+      order_status: 'paid'
+    })
+  })
+})
