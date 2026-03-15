0a224882552fb4ed0237d6d7e3dba28f45969fb4
Andr3yGabriel <andreygoncalvesdev@gmail.com>
2026-03-15 20:32:42 -0300

    feat(payment): Implementa o Gateway1Strategy usando axios

---
+++ payment-api/app/services/gateways/Gateway1Strategy.ts
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import env from '#start/env'
+import { IPaymentGateway, PaymentResult } from '#Interfaces/IPaymentGateway'
+import { PaymentDTO } from '#DTOs/PaymentDTO'
+
+export class Gateway1Strategy implements IPaymentGateway {
+    private httpClient = axios.create({
+        baseURL: env.get('GATEWAY1_URL'),
+        timeout: 5000,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+
+    async processPayment(data: PaymentDTO): Promise<PaymentResult> {
+        try {
+            const response = await this.httpClient.post('/login', {
+                email: "dev@betalent.tech",
+                token: "FEC9BB078BF338F464F96B48089EB498"
+            })
+
+            if (response.status === 200 && response.data.token) {
+                const transactionResponse = await this.httpClient.post('/transactions', {
+                    amount: data.amount,
+                    name: data.clientName,
+                    email: data.clientEmail,
+                    cardNumber: data.cardNumber,
+                    cvv: data.cvv
+                }, {
+                    headers: {
+                        'Authorization': `Bearer ${response.data.token}`
+                    }
+                })
+
+                return {
+                    success: transactionResponse.status === 201,
+                    transactionId: transactionResponse.data.transactionId
+                }
+            }
+
+            return {
+                success: false,
+                errorMessage: 'Authentication failed'
+            }
+        }
+        catch (error) {
+            return {
+                success: false,
+                errorMessage: error instanceof Error ? error.message : 'Unknown error'
+            }
+        }
+    }
+}
