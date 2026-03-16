110257f956d3685db6d9a45c4fb9d8536d40f863
Andr3yGabriel <andreygoncalvesdev@gmail.com>
2026-03-16 01:32:25 -0300

    test(functional): Adiciona caso de teste end-to-end de sucesso para o fluxo de reembolso

--- payment-api/tests/functional/functional.spec.ts
+++ payment-api/tests/functional/functional.spec.ts
@@ -3,7 +3,6 @@ import Product from '#models/product'
 import Gateway from '#models/gateway'
 import crypto from 'crypto'
 import Transaction from '#models/transaction'
-import Client from '#models/client'
 
 test.group('Checkout Transactions', (group) => {
   
@@ -34,46 +33,48 @@ test.group('Checkout Transactions', (group) => {
             cvv: "010"
         }
     }
-    const response = await client.post('/api/v1/checkout').json(payload)
+    const response  = await client.post('/api/v1/checkout').json(payload)
     response.assertBodyContains({
       message: 'Checkout successful'
     })
     assert.property(response.body(), 'transaction_id')
+
+    await Transaction.query().delete()
+    await Product.query().delete()
+    await Gateway.query().delete()
   })
 })
 
 test.group('Refund Transactions', (group) => {
-  test('should process a refund transaction succesfully via Gateway', async ({client}) => {
-    const getaway = await Gateway.create({
-      gatewayId: crypto.randomUUID(),
-      name: 'Gateway 1',
-      isActive: true,
-      priority: 1
-    })
+  test('should process a refund transaction succesfully via Gateway', async ({client, assert}) => {
+    await Gateway.create({ name: 'Gateway 1', isActive: true, priority: 1 })
+    const product = await Product.create({ name: 'Item Teste', amount: 10000 })
 
-    const dbClient = await Client.create({
-      name: 'Tester Refund',
-      email: 'refund@test.com'
-    })
-
-    const transaction = await Transaction.create({
-      transactionId: crypto.randomUUID(),
-      gatewayId: getaway.gatewayId,
-      clientId: dbClient.clientId,
-      amount: 50000,
-      status: 'paid',
-      cardLastNumbers: '6063'
-    })
-    const payload = {
-      transaction_id: transaction.transactionId,
+    const checkoutPayload = {
+        products: [{ product_id: product.productId, quantity: 1 }],
+        client: {
+            name: "Tester Refund",
+            email: "refund@example.com",
+            cardNumber: "5569000000006063",
+            cvv: "010"
+        }
     }
 
-    const response = await client.post('/api/v1/refund/:id').json(payload)
+    const checkoutResponse = await client.post('/api/v1/checkout').json(checkoutPayload)
+    const transactionId = (checkoutResponse.body() as any).transaction_id
 
-    response.assertStatus(200)
+    const response = await client.post(`/api/v1/refund/${transactionId}`)
     response.assertBodyContains({
       message: 'Refund successful',
       order_status: 'refunded'
     })
+    response.assertStatus(200)
+
+    const updatedTransaction = await Transaction.findOrFail(transactionId)
+    assert.equal(updatedTransaction.status, 'refunded')
+
+    await Transaction.query().delete()
+    await Product.query().delete()
+    await Gateway.query().delete()
   })
 })
