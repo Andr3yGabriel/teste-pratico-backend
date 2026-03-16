c7a9bb3cf3395510b96ea37eaea953c888b18e81
Andr3yGabriel <andreygoncalvesdev@gmail.com>
2026-03-15 23:39:12 -0300

    test(functional): Adiciona teste de sucesso compreensível com seeding do banco de dados

--- payment-api/tests/functional/functional.spec.ts
+++ payment-api/tests/functional/functional.spec.ts
@@ -1,33 +1,41 @@
 import { test } from '@japa/runner'
+import Product from '#models/product'
+import Gateway from '#models/gateway'
+import crypto from 'crypto'
 
 test.group('Checkout Transactions', (group) => {
+  
+  test('should process a checkout transaction successfully via Gateway', async ({ client, assert }) => {
+    const productId1 = crypto.randomUUID()
+    const productId2 = crypto.randomUUID()
 
-  test('should process a checkout transaction successfully via Gateway', async ({ client }) => {
-    const payload = {
-      "products": [
-        {
-          "product_id": 1,
-          "quantity": 2
-        },
-        {
-          "product_id": 2,
-          "quantity": 1
-        }
-      ],
-      "client": {
-        "name": "tester",
-        "email": "user@example.com",
-        "cardNumber": "5569000000006063",
-        "cvv": "010"
-      }
-    }
-
-    const response = await client.post('/api/v1/checkout').json(payload)
-
-    response.assertStatus(201)
-    response.assertBodyContains({
-      message: 'Checkout successful',
-      order_status: 'paid'
+    await Product.createMany([
+      { productId: productId1, name: 'Teclado Mecânico', amount: 35000 },
+      { productId: productId2, name: 'Mouse Gamer', amount: 15000 }
+    ])
+    await Gateway.create({
+      gatewayId: crypto.randomUUID(),
+      name: 'Gateway 1',
+      isActive: true,
+      priority: 1
     })
+    const payload = {
+        products: [
+            { product_id: productId1, quantity: 2 },
+            { product_id: productId2, quantity: 1 }
+        ],
+        client: {
+            clientId: crypto.randomUUID(),
+            name: "tester",
+            email: "user@example.com",
+            cardNumber: "5569000000006063",
+            cvv: "010"
+        }
+    }
+    const response = await client.post('/api/v1/checkout').json(payload)
+    response.assertBodyContains({
+      message: 'Checkout successful'
+    })
+    assert.property(response.body(), 'transaction_id')
   })
 })
